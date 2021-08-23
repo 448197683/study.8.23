695492b9ae51158fbe67214e79ccd9393283bb3c
448197683 <82860436+448197683@users.noreply.github.com>
2021-08-23 12:11:07 +0800

    finish

---
+++ index.js
@@ -0,0 +1,8 @@
+const scores1 = [50, 60, 70];
+const scores2 = [80, 90, 100];
+
+const scores = scores1.concat(scores2);
+
+const a = `hello, world a-b-co,ltd`;
+const b = a.split(',');
+console.log(b);
